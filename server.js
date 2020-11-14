992692e6b6ee506d612e080887122be5cfc38ee3
Benjamin JU <ben@Benjamins-MacBook-Pro.local>
2020-11-14 17:10:58 +0900

    initial Author Routes

--- server.js
+++ server.js
@@ -4,21 +4,25 @@ if(process.env.NODE_ENV !== 'production'){
 const express = require('express')
 const app = express()
 const expressLayouts = require('express-ejs-layouts')
+const bodyParser = require('body-parser')
 
 const indexRouter = require('./routes/index')
+const authorRouter = require('./routes/authors')
 
 app.set('view engine','ejs')
 app.set('views',__dirname + '/views')
 app.set('layout','layouts/layout')
 app.use(expressLayouts)
 app.use(express.static('public'))
+app.use(bodyParser.urlencoded({ limit: '10mb', extended: false }))
 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb://localhost/mybrary', { useNewUrlParser: true })
+mongoose.connect(process.env.DB_URL, { useNewUrlParser: true,useUnifiedTopology:true})
 const db = mongoose.connection
 db.on('error', error => console.error(error))
-db.once('open', () => console.log('connected to Mongoose'))
+db.once('open', () => console.log('Connected to Mongoose'))
 
 app.use('/',indexRouter)
+app.use('/authors',authorRouter)
 
 app.listen(process.env.PORT || 3001)
